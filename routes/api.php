8129a65ee77871280caece07a87b52f819bbe735
OuedraogoIbrahim <ibrahiouedraogo888@gmail.com>
2026-05-10 08:42:46 +0000

    chore: remove unused routes from web and api files

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get('events', function () {
-    return \App\Models\Event::withCount('registrations')->get();
-});
-
 
 require __DIR__ . '/events.php';
 require __DIR__ . '/registrations.php';
